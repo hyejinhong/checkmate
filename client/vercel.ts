d486c056a95df857a496010f496aa95460e2d501
Hyejin Hong <37792092+hyejinhong@users.noreply.github.com>
2026-04-15 13:31:18 +0900

    infra: vercel.ts 업데이트

--- client/vercel.ts
+++ client/vercel.ts
@@ -2,6 +2,7 @@ import { routes, deploymentEnv, type VercelConfig } from '@vercel/config/v1';
 
 export const config: VercelConfig = {
   rewrites: [
-    routes.rewrite('/api/:path*', `${deploymentEnv('ORACLE_MAIN_URL')}/api/:path*`)
+    routes.rewrite('/api/:path*', `${deploymentEnv('ORACLE_MAIN_URL')}/api/:path*`),
+    routes.rewrite('/(.*)', '/index.html'),
   ],
 };
